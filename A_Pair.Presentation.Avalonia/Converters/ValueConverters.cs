f82e74b4ced41cd645a2b2f7f6cab96b25cd0686
Helio-RC <n2567347413@163.com>
2026-05-02 22:07:52 +0800

    修复ComboBox无法显示数据的问题

--- A_Pair.Presentation.Avalonia/Converters/ValueConverters.cs
+++ A_Pair.Presentation.Avalonia/Converters/ValueConverters.cs
@@ -7,70 +7,34 @@ using Avalonia.Data.Converters;
 namespace A_Pair.Presentation.Avalonia.Converters;
 
 /// <summary>
-/// 安全转换 Gender? ↔ string，无效输入返回 null。
+/// Gender? ↔ ComboBox SelectedIndex (0=空, 1=男, 2=女, 3=其他)
 /// </summary>
-public class GenderConverter : IValueConverter
+public class GenderIndexConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is Gender g)
+        return value switch
         {
-            return g switch
+            Gender.Male => 1,
+            Gender.Female => 2,
+            Gender.Other => 3,
+            _ => 0
+        };
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int i)
+        {
+            return i switch
             {
-                Gender.Male => "男",
-                Gender.Female => "女",
-                Gender.Other => "其他",
-                _ => g.ToString()
+                1 => Gender.Male,
+                2 => Gender.Female,
+                3 => Gender.Other,
+                _ => (Gender?)null
             };
         }
-        return "";
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        var s = value switch
-        {
-            global::Avalonia.Controls.ComboBoxItem item => item.Content?.ToString(),
-            string str => str,
-            _ => null
-        };
-
-        if (string.IsNullOrWhiteSpace(s))
-            return null;
-
-        s = s.Trim();
-        return s switch
-        {
-            "Male" or "male" or "男" or "男性" => Gender.Male,
-            "Female" or "female" or "女" or "女性" => Gender.Female,
-            "Other" or "other" or "其他" => Gender.Other,
-            _ => null
-        };
-    }
-}
-
-/// <summary>
-/// 安全转换 bool ↔ string（是/否），无效输入返回 false。
-/// </summary>
-public class NeedsFrontRowConverter : IValueConverter
-{
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        return value is true ? "是" : "否";
-    }
-
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-    {
-        var s = value as string;
-        if (string.IsNullOrWhiteSpace(s))
-            return false;
-
-        s = s.Trim();
-        return s switch
-        {
-            "true" or "True" or "TRUE" or "是" or "Y" or "y" or "1" => true,
-            _ => false
-        };
+        return null;
     }
 }
 
