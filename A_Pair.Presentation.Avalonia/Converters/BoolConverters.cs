c91fa0862a1d7066a3d39ac22add6ca1493753ff
Helio-RC <n2567347413@163.com>
2026-05-02 16:58:22 +0800

    动画

---
+++ A_Pair.Presentation.Avalonia/Converters/BoolConverters.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace A_Pair.Presentation.Avalonia.Converters;
+
+public static class BoolConverters
+{
+    public static readonly IValueConverter TrueToVisible = new BoolToDoubleConverter(1, 0);
+    public static readonly IValueConverter FalseToVisible = new BoolToDoubleConverter(0, 1);
+
+    private class BoolToDoubleConverter(double trueValue, double falseValue) : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => value is true ? trueValue : falseValue;
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
