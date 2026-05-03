0421143acfa252de70bcbfca4521b56490a2eed3
Helio-RC <n2567347413@163.com>
2026-05-03 22:19:21 +0800

    FIX数据预览问题

--- A_Pair.Presentation.Avalonia/Views/DataManagementView.axaml.cs
+++ A_Pair.Presentation.Avalonia/Views/DataManagementView.axaml.cs
@@ -1,3 +1,5 @@
+using System;
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 
@@ -5,9 +7,48 @@ namespace A_Pair.Presentation.Avalonia.Views;
 
 public partial class DataManagementView : UserControl
 {
+    private ViewModels.DataManagementViewModel? _vm;
+
     public DataManagementView ()
     {
         InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged (EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (_vm != null)
+            _vm.PropertyChanged -= OnSidebarWidthChanged;
+        _vm = DataContext as ViewModels.DataManagementViewModel;
+        if (_vm != null)
+        {
+            _vm.PropertyChanged += OnSidebarWidthChanged;
+            SyncSidebar(_vm.SidebarListWidth);
+        }
+    }
+
+    private void OnSidebarWidthChanged (object? sender , PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ViewModels.DataManagementViewModel.SidebarListWidth)
+            && sender is ViewModels.DataManagementViewModel vm)
+        {
+            SyncSidebar(vm.SidebarListWidth);
+        }
+    }
+
+    private void SyncSidebar (double width)
+    {
+        // Find the content Grid (child of outermost DockPanel, first Grid with ColumnDefinitions)
+        var root = (Content as DockPanel) ?? (VisualChildren.Count > 0 ? VisualChildren[0] as DockPanel : null);
+        if (root == null) return;
+        foreach (var child in root.Children)
+        {
+            if (child is Grid grid && grid.ColumnDefinitions.Count >= 3)
+            {
+                grid.ColumnDefinitions[0].Width = new GridLength(width);
+                break;
+            }
+        }
     }
 
     protected override void OnPropertyChanged (AvaloniaPropertyChangedEventArgs change)
--- A_Pair.Presentation.Avalonia/Views/VenueConfigurationView.axaml.cs
+++ A_Pair.Presentation.Avalonia/Views/VenueConfigurationView.axaml.cs
@@ -1,23 +1,62 @@
+using System;
+using System.ComponentModel;
 using Avalonia;
 using Avalonia.Controls;
 
-namespace A_Pair.Presentation.Avalonia.Views
+namespace A_Pair.Presentation.Avalonia.Views;
+
+public partial class VenueConfigurationView : UserControl
 {
-    public partial class VenueConfigurationView : UserControl
+    private ViewModels.VenueConfigurationViewModel? _vm;
+
+    public VenueConfigurationView ()
     {
-        public VenueConfigurationView ()
+        InitializeComponent();
+    }
+
+    protected override void OnDataContextChanged (EventArgs e)
+    {
+        base.OnDataContextChanged(e);
+        if (_vm != null)
+            _vm.PropertyChanged -= OnSidebarWidthChanged;
+        _vm = DataContext as ViewModels.VenueConfigurationViewModel;
+        if (_vm != null)
         {
-            InitializeComponent();
+            _vm.PropertyChanged += OnSidebarWidthChanged;
+            SyncSidebar(_vm.SidebarListWidth);
         }
+    }
 
-        protected override void OnPropertyChanged (AvaloniaPropertyChangedEventArgs change)
+    private void OnSidebarWidthChanged (object? sender , PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(ViewModels.VenueConfigurationViewModel.SidebarListWidth)
+            && sender is ViewModels.VenueConfigurationViewModel vm)
         {
-            base.OnPropertyChanged(change);
+            SyncSidebar(vm.SidebarListWidth);
+        }
+    }
 
-            if (change.Property == BoundsProperty && DataContext is ViewModels.VenueConfigurationViewModel vm)
+    private void SyncSidebar (double width)
+    {
+        var root = Content as DockPanel;
+        if (root == null) return;
+        foreach (var child in root.Children)
+        {
+            if (child is Grid grid && grid.ColumnDefinitions.Count >= 3)
             {
-                vm.OnWindowWidthChanged(Bounds.Width);
+                grid.ColumnDefinitions[0].Width = new GridLength(width);
+                break;
             }
+        }
+    }
+
+    protected override void OnPropertyChanged (AvaloniaPropertyChangedEventArgs change)
+    {
+        base.OnPropertyChanged(change);
+
+        if (change.Property == BoundsProperty && DataContext is ViewModels.VenueConfigurationViewModel vm)
+        {
+            vm.OnWindowWidthChanged(Bounds.Width);
         }
     }
 }
